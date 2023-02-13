7bdda162f2c816d63c9319f2d924c39ac7dd5010
bug41 <bugkinguswit@gmail.com>
2023-02-13 23:21:53 +0900

    db 접속정보 제거

--- common/include.php
+++ common/include.php
@@ -1,16 +1,15 @@
 <?php
     //1.데이터베이스 연결
-    $dbservername = '222.239.14.105';
-    $dbusername = 'rookie_user_gabin';
-    $dbuserpassword = 'fnzlWkd!!!';
-    $dbname = 'rookie_gabin';
+    $dbservername = '';
+    $dbusername = '';
+    $dbuserpassword = '';
+    $dbname = '';
 
-	$conn = mysqli_connect($dbservername,$dbusername,$dbuserpassword) or die( "SQL server에 연결할 수 없습니다~~~");	
-	mysqli_select_db($conn,$dbname);
-    //mysqli_query($connect,"SET NAMES 'utf8'");
+    $conn = mysqli_connect($dbservername,$dbusername,$dbuserpassword) or die( "SQL server에 연결할 수 없습니다~~~");	
+    mysqli_select_db($conn,$dbname);    
     mysqli_set_charset($conn, 'utf8mb4');
-    
+
     if (mysqli_connect_errno()) {
-	    throw new Exception(mysqli_connect_error(), mysqli_connect_errno());
-	}
+        throw new Exception(mysqli_connect_error(), mysqli_connect_errno());
+    }
 ?>
